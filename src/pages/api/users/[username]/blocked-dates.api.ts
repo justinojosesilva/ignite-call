d2d2f76dcb08c8feff85ab6ec093daa9d5b46603
Justino <justinojosesilva@gmail.com>
2024-09-11 21:33:40 -0300

    Commit project Ignite Call

---
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,73 @@
+/* eslint-disable camelcase */
+import { prisma } from '@/src/lib/prisma'
+import { Prisma } from '@prisma/client'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { year, month } = req.query
+
+  if (!year || !month) {
+    return res.status(400).json({ message: 'Year or month not specified.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    select: {
+      week_day: true,
+    },
+    where: {
+      user_id: user.id,
+    },
+  })
+
+  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+    return !availableWeekDays.some(
+      (availableWeekDay) => availableWeekDay.week_day === weekDay,
+    )
+  })
+
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT
+      EXTRACT(DAY FROM S.date) AS date,
+      COUNT(S.date) AS amount,
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+    FROM schedulings S
+
+    LEFT JOIN user_time_intervals UTI 
+      ON UTI.week_day = EXTRACT(DOW FROM S.date + INTERVAL '1 day')
+
+    WHERE S.user_id = ${user.id}
+      AND EXTRACT(YEAR FROM S.date) = ${year}::int 
+      AND EXTRACT(MONTH FROM S.date) = ${month}::int
+
+    GROUP BY 
+      EXTRACT(DAY FROM S.date),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING COUNT(S.date) >= ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60);
+  `
+
+  const blockedDates = blockedDatesRaw.map((item) => Number(item.date))
+
+  console.log(blockedDates)
+
+  return res.json({ blockedWeekDays, blockedDates })
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,90 @@
+import { Calendar } from '@/src/components/Calendar'
+import {
+  Container,
+  TimerPicker,
+  TimerPickerHeader,
+  TimerPickerItem,
+  TimerPickerList,
+} from './styles'
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import { api } from '@/src/lib/axios'
+import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
+
+interface Availability {
+  possibleTimes: number[]
+  availableTimes: number[]
+}
+
+interface CalendarStepProps {
+  onSelectDateTime: (date: Date) => void
+}
+
+export function CalendarStep({ onSelectDateTime }: CalendarStepProps) {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  // const [availability, setAvailability] = useState<Availability | null>(null)
+
+  const router = useRouter()
+  const isDateSelected = !!selectedDate
+  const username = String(router.query.username)
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
+
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
+
+  const { data: availability } = useQuery<Availability>({
+    queryKey: ['availability', selectedDateWithoutTime],
+    queryFn: async () => {
+      const response = await api.get(`users/${username}/availability`, {
+        params: {
+          date: selectedDateWithoutTime,
+        },
+      })
+      return response.data
+    },
+    enabled: !!selectedDate,
+  })
+
+  function handleSelectTime(hour: number) {
+    const dateWithTime = dayjs(selectedDate)
+      .set('hour', hour)
+      .startOf('hour')
+      .toDate()
+
+    onSelectDateTime(dateWithTime)
+  }
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
+
+      {isDateSelected && (
+        <TimerPicker>
+          <TimerPickerHeader>
+            {weekDay} <span>{describedDate}</span>
+          </TimerPickerHeader>
+
+          <TimerPickerList>
+            {availability?.possibleTimes.map((hour) => {
+              return (
+                <TimerPickerItem
+                  key={hour}
+                  onClick={() => handleSelectTime(hour)}
+                  disabled={!availability.availableTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, '0')}:00h
+                </TimerPickerItem>
+              )
+            })}
+          </TimerPickerList>
+        </TimerPicker>
+      )}
+    </Container>
+  )
+}
